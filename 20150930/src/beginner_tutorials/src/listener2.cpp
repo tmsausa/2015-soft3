099aaad7f04dc94ccbe55464d756566a2802b517
tmsausa <tmsausa@gmail.com>
2015-10-25 14:56:54 +0900

    add 2015-09-29 shukudai

--- 20150930/src/beginner_tutorials/src/listener2.cpp
+++ 20150930/src/beginner_tutorials/src/listener2.cpp
@@ -4,15 +4,14 @@
 
 void chatterCallback(const beginner_tutorials::Hello::ConstPtr& msg)
 {
-  ROS_INFO("I heard: [%s %f %f %f]", msg->hello.c_str(),
-	   msg->pos.x, msg->pos.y, msg->pos.z);
+  ROS_INFO("I heard: [%s %f %f %f]",msg->hello.c_str(),msg->pos.x,msg->pos.y,msg->pos.z);
 }
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "listener2");
+  ros::init(argc,argv,"listener2");
   ros::NodeHandle n;
-  ros::Subscriber sub = n.subscribe("chatter2", 1000, chatterCallback);
+  ros::Subscriber sub=n.subscribe("chatter2",1000,chatterCallback);
   ros::spin();
   return 0;
 }
